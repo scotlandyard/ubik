5ebb6222e460494568a1d933428970a78cad4ec5
vauxhall <zero.griffin@gmail.com>
2016-06-28 16:37:20 -0500

    app directory

--- ubik/Category/CategoryNSFileManager.swift
+++ ubik/Category/CategoryNSFileManager.swift
@@ -10,5 +10,11 @@ import Foundation
 
 extension NSFileManager
 {
-    
+    class func appDirectory() -> NSURL
+    {
+        let urls:[NSURL] = NSFileManager.defaultManager().URLsForDirectory(NSSearchPathDirectory.DocumentDirectory, inDomains:NSSearchPathDomainMask.UserDomainMask)
+        let appDirectory:NSURL = urls.last!
+        
+        return appDirectory
+    }
 }
