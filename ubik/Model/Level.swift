e71cb19047fb493468a895f47be81c71349d49e4
vauxhall <zero.griffin@gmail.com>
2016-06-28 18:23:34 -0500

    models

---
+++ ubik/Model/Level.swift
@@ -0,0 +1,17 @@
+//
+//  Level.swift
+//  ubik
+//
+//  Created by zero on 6/28/16.
+//  Copyright © 2016 zero. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+class Level: NSManagedObject {
+
+// Insert code here to add functionality to your managed object subclass
+
+}
