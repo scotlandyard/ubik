dedb305163cc4e87ab1a0a8c01979470d9c57e23
vauxhall <zero.griffin@gmail.com>
2016-06-30 20:45:37 -0500

    metal integration

--- ubik/Controller/Main/CMainParent.swift
+++ ubik/Controller/Main/CMainParent.swift
@@ -1,13 +1,36 @@
 import UIKit
+import Metal
 
 class CMainParent:UIViewController
 {
     let dManager:DManager = DManager()
+    
+    var device:MTLDevice!
+    var metalLayer:CAMetalLayer!
+    let vertexData:[Float] = [
+        0.0, 1.0, 0.0,
+        -1.0, -1.0, 0.0,
+        1.0, -1.0, 0.0]
+    var vertexBuffer:MTLBuffer! = nil
     
     override func viewDidLoad()
     {
         let level:DModelLevel = dManager.fetchLevel()
         level.steps += 1
         dManager.saveContext()
+        
+        
+        
+        device = MTLCreateSystemDefaultDevice()
+        metalLayer = CAMetalLayer()
+        metalLayer.device = device
+        metalLayer.pixelFormat = MTLPixelFormat.BGRA8Unorm
+        metalLayer.framebufferOnly = true
+        metalLayer.frame = view.layer.frame
+        
+        view.layer.addSublayer(metalLayer)
+        
+        let dataSize:Int = vertexData.count * sizeofValue(vertexData[0])
+        vertexBuffer = device.newBufferWithBytes(vertexData, length:dataSize, options: MTLResourceOptions.OptionCPUCacheModeDefault)
     }
 }
