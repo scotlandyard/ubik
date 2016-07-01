1e2b4346c0a69ad79c50e8614b569ab07169b52b
vauxhall <zero.griffin@gmail.com>
2016-07-01 15:25:09 -0500

    component gyro

--- ubik/View/Component/VComponentGyro.swift
+++ ubik/View/Component/VComponentGyro.swift
@@ -18,8 +18,26 @@ class VComponentGyro:UIView
     
     override func drawRect(rect:CGRect)
     {
+        let width:CGFloat = rect.maxX
+        let height:CGFloat = rect.maxY
+        let width_2:CGFloat = width / 2
+        let height_2:CGFloat = height / 2
+        let size:CGFloat
+        
+        if width > height
+        {
+            size = height_2 - model.lineWidth
+        }
+        else
+        {
+            size = width_2 - model.lineWidth
+        }
+        
         let context:CGContext = UIGraphicsGetCurrentContext()!
         CGContextSetLineWidth(context, model.lineWidth)
+        CGContextSetStrokeColorWithColor(context, UIColor.complement().CGColor)
+        CGContextAddArc(context, width_2, height_2, size, 0.0001, 0, 0)
+        CGContextDrawPath(context, CGPathDrawingMode.Stroke)
         CGContextSetStrokeColorWithColor(context, model.color.CGColor)
     }
 }
