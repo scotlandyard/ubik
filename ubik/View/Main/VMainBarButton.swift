0d77146e2c8d65f80da3d7ab49d5ad335d22e488
vauxhall <zero.griffin@gmail.com>
2016-07-02 22:03:49 -0500

    history factory button

--- ubik/View/Main/VMainBarButton.swift
+++ ubik/View/Main/VMainBarButton.swift
@@ -2,5 +2,10 @@ import UIKit
 
 class VMainBarButton:UIButton
 {
-    
+    class func History() -> VMainBarButton
+    {
+        let button:VMainBarButton = VMainBarButtonHistory()
+        
+        return button
+    }
 }
---
+++ ubik/View/Main/VMainBarButtonHistory.swift
@@ -0,0 +1,6 @@
+import UIKit
+
+class VMainBarButtonHistory:VMainBarButton
+{
+    
+}
