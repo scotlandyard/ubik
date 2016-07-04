49fd4c8aea1458fedca9bdaf0a5fd1817ad66791
vauxhall <zero.griffin@gmail.com>
2016-07-04 00:14:20 -0500

    cleaned objects

--- ubik/Data/Object/Session/DObjectSessionExperience+CoreDataProperties.swift
+++ ubik/Data/Object/Session/DObjectSessionExperience+CoreDataProperties.swift
@@ -1,20 +1,8 @@
-//
-//  DObjectSessionExperience+CoreDataProperties.swift
-//  ubik
-//
-//  Created by zero on 7/4/16.
-//  Copyright © 2016 zero. All rights reserved.
-//
-//  Choose "Create NSManagedObject Subclass…" from the Core Data editor menu
-//  to delete and recreate this implementation file for your updated model.
-//
-
 import Foundation
 import CoreData
 
-extension DObjectSessionExperience {
-
-    @NSManaged var version: String?
-    @NSManaged var onboardingDone: Bool
-
+extension DObjectSessionExperience
+{
+    @NSManaged var version:String
+    @NSManaged var onboardingDone:Bool
 }
--- ubik/Data/Object/Session/DObjectSessionExperience.swift
+++ ubik/Data/Object/Session/DObjectSessionExperience.swift
@@ -1,17 +1,7 @@
-//
-//  DObjectSessionExperience.swift
-//  ubik
-//
-//  Created by zero on 7/4/16.
-//  Copyright © 2016 zero. All rights reserved.
-//
-
 import Foundation
 import CoreData
 
 @objc(DObjectSessionExperience)
-class DObjectSessionExperience: NSManagedObject {
-
-// Insert code here to add functionality to your managed object subclass
-
+class DObjectSessionExperience:NSManagedObject
+{
 }
