211ba2d628dbd7a46426a6a2ef7a98c7eeceed9c
vauxhall <zero.griffin@gmail.com>
2016-07-04 09:58:22 -0500

    onboarding items

---
+++ ubik/Model/Onboarding/MOnboardingItemLoading.swift
@@ -0,0 +1,9 @@
+//
+//  MOnboardingItemLoading.swift
+//  ubik
+//
+//  Created by zero on 7/4/16.
+//  Copyright © 2016 zero. All rights reserved.
+//
+
+import Foundation
---
+++ ubik/Model/Onboarding/MOnboardingItemReady.swift
@@ -0,0 +1,9 @@
+//
+//  MOnboardingItemReady.swift
+//  ubik
+//
+//  Created by zero on 7/4/16.
+//  Copyright © 2016 zero. All rights reserved.
+//
+
+import Foundation
