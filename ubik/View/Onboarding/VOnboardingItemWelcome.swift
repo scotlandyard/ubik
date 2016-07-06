7c14287af08a50cd46776c8b2fef668c8b73b221
vauxhall <zero.griffin@gmail.com>
2016-07-05 22:00:58 -0500

    added logo asset

--- ubik/View/Onboarding/VOnboardingItemWelcome.swift
+++ ubik/View/Onboarding/VOnboardingItemWelcome.swift
@@ -8,5 +8,32 @@ class VOnboardingItemWelcome:UIView
     {
         self.init()
         self.controller = controller
+        backgroundColor = UIColor.clearColor()
+        clipsToBounds = true
+        
+        let icon:UIImageView = UIImageView()
+        icon.userInteractionEnabled = false
+        icon.translatesAutoresizingMaskIntoConstraints = false
+        icon.clipsToBounds = true
+        icon.contentMode = UIViewContentMode.Center
+        icon.image = UIImage(named:"icon")
+        
+        addSubview(icon)
+        
+        let views:[String:AnyObject] = [
+            "icon":icon]
+        
+        let metrics:[String:AnyObject] = [:]
+        
+        addConstraints(NSLayoutConstraint.constraintsWithVisualFormat(
+            "H:|-0-[icon]-0-|",
+            options:[],
+            metrics:metrics,
+            views:views))
+        addConstraints(NSLayoutConstraint.constraintsWithVisualFormat(
+            "V:|-0-[icon(200)]",
+            options:[],
+            metrics:metrics,
+            views:views))
     }
 }
