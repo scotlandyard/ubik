26c079af7ffc21a22c65bdf6b9d97d0e5ac461b4
vauxhall <zero.griffin@gmail.com>
2016-07-08 20:54:15 -0500

    manager model steps

---
+++ ubik/Data/Manager/DManagerModelSteps.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+class DManagerModelSteps:DManagerModel
+{
+    let kEntity_Hike:String = "Hike"
+    private let kModelName:String = "DSessionSteps"
+    
+    init()
+    {
+        super.init(modelName:kModelName)
+    }
+}
