8c632f0858c94cb4480d191da2b7e12a0ce6fea3
vauxhall <zero.griffin@gmail.com>
2016-07-08 20:48:05 -0500

    improved managed objects

--- ubik/Data/Object/Steps/DStepsHike+CoreDataProperties.swift
+++ ubik/Data/Object/Steps/DStepsHike+CoreDataProperties.swift
@@ -1,21 +1,9 @@
-//
-//  DStepsHike+CoreDataProperties.swift
-//  ubik
-//
-//  Created by zero on 7/8/16.
-//  Copyright © 2016 zero. All rights reserved.
-//
-//  Choose "Create NSManagedObject Subclass…" from the Core Data editor menu
-//  to delete and recreate this implementation file for your updated model.
-//
-
 import Foundation
 import CoreData
 
-extension DStepsHike {
-
-    @NSManaged var day: NSTimeInterval
-    @NSManaged var amount: Int16
-    @NSManaged var milestone: Bool
-
+extension DStepsHike
+{
+    @NSManaged var day:NSTimeInterval
+    @NSManaged var amount:Int16
+    @NSManaged var milestone:Bool
 }
--- ubik/Data/Object/Steps/DStepsHike.swift
+++ ubik/Data/Object/Steps/DStepsHike.swift
@@ -1,17 +1,7 @@
-//
-//  DStepsHike.swift
-//  ubik
-//
-//  Created by zero on 7/8/16.
-//  Copyright © 2016 zero. All rights reserved.
-//
-
 import Foundation
 import CoreData
 
 @objc(DStepsHike)
-class DStepsHike: NSManagedObject {
-
-// Insert code here to add functionality to your managed object subclass
-
+class DStepsHike:NSManagedObject
+{
 }
