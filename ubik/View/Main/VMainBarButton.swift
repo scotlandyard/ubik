f237a3916804570fa7c00b6098bef84ba7376ab0
vauxhall <zero.griffin@gmail.com>
2016-07-10 21:32:28 -0500

    button hover

--- ubik/View/Main/VMainBarButton.swift
+++ ubik/View/Main/VMainBarButton.swift
@@ -3,6 +3,7 @@ import UIKit
 class VMainBarButton:UIButton
 {
     weak var label:UILabel!
+    private var active:Bool
     
     class func Summary() -> VMainBarButton
     {
@@ -27,20 +28,18 @@ class VMainBarButton:UIButton
     
     init(image:String, title:String)
     {
+        active = false
         super.init(frame:CGRectZero)
         translatesAutoresizingMaskIntoConstraints = false
         clipsToBounds = true
-        setImage(UIImage(named:image)?.imageWithRenderingMode(UIImageRenderingMode.AlwaysOriginal), forState:UIControlState.Normal)
-        setImage(UIImage(named:image)?.imageWithRenderingMode(UIImageRenderingMode.AlwaysTemplate), forState:UIControlState.Highlighted)
+        setImage(UIImage(named:image)?.imageWithRenderingMode(UIImageRenderingMode.AlwaysTemplate), forState:UIControlState.Normal)
         imageView?.contentMode = UIViewContentMode.Center
-        imageView?.tintColor = UIColor(white:0, alpha:0.1)
         imageView?.clipsToBounds = true
         imageEdgeInsets = UIEdgeInsetsMake(18, 0, 12, 0)
         
         let label:UILabel = UILabel()
         label.userInteractionEnabled = false
         label.translatesAutoresizingMaskIntoConstraints = false
-        label.textColor = UIColor.blackColor()
         label.text = title
         label.textAlignment = NSTextAlignment.Center
         label.font = UIFont.regular(11)
@@ -63,10 +62,55 @@ class VMainBarButton:UIButton
             options:[],
             metrics:metrics,
             views:views))
+        
+        hover()
     }
     
     required init?(coder:NSCoder)
     {
         fatalError()
     }
+    
+    override var selected:Bool
+    {
+        didSet
+        {
+            hover()
+        }
+    }
+    
+    override var highlighted:Bool
+    {
+        didSet
+        {
+            hover()
+        }
+    }
+    
+    //MARK: private
+    
+    private func hover()
+    {
+        let color:UIColor
+        
+        if active
+        {
+            color = UIColor.main()
+        }
+        else if selected || highlighted
+        {
+            color = UIColor(white:0, alpha:0.1)
+        }
+        else
+        {
+            color = UIColor.blackColor()
+        }
+        
+        imageView?.tintColor = color
+        label.textColor = color
+    }
+    
+    //MARK: public
+    
+    
 }
