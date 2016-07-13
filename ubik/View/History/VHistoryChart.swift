c3658d29f7adb176ef18e5b3307ae8d3519e27ae
vauxhall <zero.griffin@gmail.com>
2016-07-12 20:30:47 -0500

    category reusable view

--- ubik/View/History/VHistoryChart.swift
+++ ubik/View/History/VHistoryChart.swift
@@ -1,6 +1,29 @@
 import UIKit
 
-class VHistoryChart:UIView
+class VHistoryChart:UIView, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewFlowLayout
 {
+    init()
+    {
+        super.init(frame:CGRectZero)
+        translatesAutoresizingMaskIntoConstraints = false
+        clipsToBounds = true
+        
+        
+    }
     
+    //MARK: col del
+    
+    func numberOfSectionsInCollectionView(collectionView:UICollectionView) -> Int
+    {
+        return 1
+    }
+    
+    func collectionView(collectionView:UICollectionView, numberOfItemsInSection section:Int) -> Int
+    {
+        return 0
+    }
+    
+    func collectionView(collectionView:UICollectionView, cellForItemAtIndexPath indexPath:NSIndexPath) -> UICollectionViewCell
+    {
+    }
 }
---
+++ ubik/View/History/VHistoryChartCell.swift
@@ -0,0 +1,6 @@
+import UIKit
+
+class VHistoryChartCell:UICollectionViewCell
+{
+    
+}
