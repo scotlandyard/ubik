4b6aa6948655f3d0ace164497119042ff3d970ac
vauxhall <zero.griffin@gmail.com>
2016-07-18 11:03:55 -0500

    settings header

--- ubik/View/Settings/VSettings.swift
+++ ubik/View/Settings/VSettings.swift
@@ -3,6 +3,7 @@ import UIKit
 class VSettings:UIView
 {
     weak var controller:CSettings!
+    weak var collection:UICollectionView!
     
     convenience init(controller:CSettings)
     {
@@ -10,5 +11,20 @@ class VSettings:UIView
         clipsToBounds = true
         backgroundColor = UIColor.whiteColor()
         self.controller = controller
+        
+        let flow:UICollectionViewFlowLayout = UICollectionViewFlowLayout()
+        flow.footerReferenceSize = CGSizeZero
+        flow.minimumLineSpacing = 0
+        flow.minimumInteritemSpacing = 0
+        flow.scrollDirection = UICollectionViewScrollDirection.Vertical
+        flow.sectionInset = UIEdgeInsetsZero
+        
+        let collection:UICollectionView = UICollectionView(frame:CGRectZero, collectionViewLayout:flow)
+        collection.clipsToBounds = true
+        collection.backgroundColor = UIColor.clearColor()
+        collection.translatesAutoresizingMaskIntoConstraints = false
+        self.collection = collection
+        
+        addSubview(collection)
     }
 }
