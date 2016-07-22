c85d35acb0cfba6505d4d87c67780d0d808f747c
vauxhall <zero.griffin@gmail.com>
2016-07-21 21:22:19 -0500

    models component pay

---
+++ ubik/Model/Component/Pay/MComponentPaySteps.swift
@@ -0,0 +1,6 @@
+import Foundation
+
+class MComponentPaySteps:MComponentPay
+{
+    
+}
--- ubik/View/Component/VComponentPay.swift
+++ ubik/View/Component/VComponentPay.swift
@@ -2,5 +2,21 @@ import UIKit
 
 class VComponentPay:UIView
 {
+    init(model:MComponentGyro)
+    {
+        self.model = model
+        currentRadius = kMinRadius
+        
+        super.init(frame:CGRectZero)
+        
+        clipsToBounds = true
+        userInteractionEnabled = false
+        backgroundColor = UIColor.clearColor()
+        translatesAutoresizingMaskIntoConstraints = false
+    }
     
+    required init?(coder aDecoder:NSCoder)
+    {
+        fatalError()
+    }
 }
