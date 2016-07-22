1d15401e3ad69094afe48ec6d2aba6e2d111776d
vauxhall <zero.griffin@gmail.com>
2016-07-21 21:37:27 -0500

    factory steps and time

--- ubik/Model/Summary/MSummaryItem.swift
+++ ubik/Model/Summary/MSummaryItem.swift
@@ -2,5 +2,17 @@ import Foundation
 
 class MSummaryItem
 {
+    class func Steps() -> MSummaryItem
+    {
+        let model:MSummaryItem = MSummaryItemSteps()
+        
+        return model
+    }
     
+    class func Item() -> MSummaryItem
+    {
+        let model:MSummaryItem = MSummaryItemItem()
+        
+        return model
+    }
 }
