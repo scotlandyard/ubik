26fa131941deabc0640936afb118f829e34355b1
vauxhall <zero.griffin@gmail.com>
2016-07-23 11:57:49 -0500

    register for notifications

--- ubik/Controller/Onboarding/COnboardingItemReady.swift
+++ ubik/Controller/Onboarding/COnboardingItemReady.swift
@@ -7,6 +7,14 @@ class COnboardingItemReady:COnboardingItem
         view = VOnboardingItemReady(controller:self)
     }
     
+    override func viewDidAppear(animated:Bool)
+    {
+        super.viewDidAppear(animated)
+        
+        let settings:UIUserNotificationSettings = UIUserNotificationSettings(forTypes:[.Alert, .Badge, .Sound], categories:nil)
+        UIApplication.sharedApplication().registerUserNotificationSettings(settings)
+    }
+    
     //MARK: public
     
     func onboardingDone()
