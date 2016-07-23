14a52dc565deff3071ae4ba314413208961a8045
vauxhall <zero.griffin@gmail.com>
2016-07-23 16:03:52 -0500

    using components from factory

--- ubik/Model/History/MHistoryItem.swift
+++ ubik/Model/History/MHistoryItem.swift
@@ -2,22 +2,27 @@ import UIKit
 
 class MHistoryItem
 {
-    let distance:String
-    let steps:String
-    let ratio:String
-    let date:String
     let percentage:CGFloat
     let components:[MHistoryItemComponent<VHistoryChartDisplayCell>]
     
     init(distance:String, steps:String, ratio:String, date:String, percentage:CGFloat)
     {
-        self.distance = distance
-        self.steps = steps
-        self.ratio = ratio
-        self.date = date
         self.percentage = percentage
         
+        let itemDate:MHistoryItemComponent<VHistoryChartDisplayCell> = MHistoryItemComponent.Date(date)
+        let itemAmountDistance:MHistoryItemComponent<VHistoryChartDisplayCell> = MHistoryItemComponent.AmountDistance(distance)
+        let itemTitleDistance:MHistoryItemComponent<VHistoryChartDisplayCell> = MHistoryItemComponent.TitleDistance()
+        let itemAmountSteps:MHistoryItemComponent<VHistoryChartDisplayCell> = MHistoryItemComponent.AmountSteps(steps)
+        let itemTitleSteps:MHistoryItemComponent<VHistoryChartDisplayCell> = MHistoryItemComponent.TitleSteps()
+        let itemRatio:MHistoryItemComponent<VHistoryChartDisplayCell> = MHistoryItemComponent.Ratio(ratio)
+        
         components = [
+            itemDate,
+            itemAmountDistance,
+            itemTitleDistance,
+            itemAmountSteps,
+            itemTitleSteps,
+            itemRatio
         ]
     }
 }
--- ubik/Model/History/MHistoryItemComponent.swift
+++ ubik/Model/History/MHistoryItemComponent.swift
@@ -5,50 +5,50 @@ class MHistoryItemComponent<CellType:VHistoryChartDisplayCell>
     let reusableIdentifier:String
     let cellHeight:CGFloat
     
-    class func Date(date:String) -> MHistoryItemComponent
+    class func Date(date:String) -> MHistoryItemComponent<CellType>
     {
         let item:AnyObject = MHistoryItemComponentDate(date:date)
-        let itemComponent:MHistoryItemComponent = item as! MHistoryItemComponent
+        let itemComponent:MHistoryItemComponent<CellType> = item as! MHistoryItemComponent
         
         return itemComponent
     }
     
-    class func AmountDistance(distance:String) -> MHistoryItemComponent
+    class func AmountDistance(distance:String) -> MHistoryItemComponent<CellType>
     {
         let item:AnyObject = MHistoryItemComponentAmountDistance(distance:distance)
-        let itemComponent:MHistoryItemComponent = item as! MHistoryItemComponent
+        let itemComponent:MHistoryItemComponent<CellType> = item as! MHistoryItemComponent
         
         return itemComponent
     }
     
-    class func TitleDistance() -> MHistoryItemComponent
+    class func TitleDistance() -> MHistoryItemComponent<CellType>
     {
         let item:AnyObject = MHistoryItemComponentTitleDistance()
-        let itemComponent:MHistoryItemComponent = item as! MHistoryItemComponent
+        let itemComponent:MHistoryItemComponent<CellType> = item as! MHistoryItemComponent
         
         return itemComponent
     }
     
-    class func AmountSteps(steps:String) -> MHistoryItemComponent
+    class func AmountSteps(steps:String) -> MHistoryItemComponent<CellType>
     {
         let item:AnyObject = MHistoryItemComponentAmountSteps(steps:steps)
-        let itemComponent:MHistoryItemComponent = item as! MHistoryItemComponent
+        let itemComponent:MHistoryItemComponent<CellType> = item as! MHistoryItemComponent
         
         return itemComponent
     }
     
-    class func TitleSteps() -> MHistoryItemComponent
+    class func TitleSteps() -> MHistoryItemComponent<CellType>
     {
         let item:AnyObject = MHistoryItemComponentTitleSteps()
-        let itemComponent:MHistoryItemComponent = item as! MHistoryItemComponent
+        let itemComponent:MHistoryItemComponent<CellType> = item as! MHistoryItemComponent
         
         return itemComponent
     }
     
-    class func Ratio(ratio:String) -> MHistoryItemComponent
+    class func Ratio(ratio:String) -> MHistoryItemComponent<CellType>
     {
         let item:AnyObject = MHistoryItemComponentRatio(ratio:ratio)
-        let itemComponent:MHistoryItemComponent = item as! MHistoryItemComponent
+        let itemComponent:MHistoryItemComponent<CellType> = item as! MHistoryItemComponent
         
         return itemComponent
     }
